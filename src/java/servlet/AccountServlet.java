166a0a6cca7aa6c10b2789b29a79a37921e1a0c6
kamaljit <c0687969@mylambton.ca>
2017-02-15 11:52:18 -0500

    "Final Test "

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package servlet;
-
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,35 +23,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Account;
 
-
 /**
  * Provides an Account Balance and Basic Withdrawal/Deposit Operations
  */
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
+
     private Account account = new Account();
-    
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
-        try(PrintWriter out = response.getWriter()){
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        try (PrintWriter out = response.getWriter()) {
+            response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
             out.println(account.getBalance());
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.err.println("Something went wrong: " + ex.getMessage());
         }
     }
+
     @Override
-     protected void doPost(HttpServletRequest request, HttpServletResponse response){
-         String deposit = request.getParameter("deposit");
-         double deposit1 = Double.parseDouble(deposit);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        //String deposit = request.getParameter("deposit");
+       //double deposit1 = Double.parseDouble(deposit);
         // double desposit = Double.parseDouble(request.getParameter("deposit"));
-         account.deposit(deposit1);
-        try(PrintWriter out = response.getWriter()){
+      //  account.deposit(deposit1);
+        try (PrintWriter out = response.getWriter()) {
+            if (request.getParameter("withdraw") != null) {
+                double withdraw = Double.parseDouble(request.getParameter("withdraw"));
+                account.withdraw(withdraw);
+            } else if (request.getParameter("deposit")!= null) {
+                double deposit=Double.parseDouble(request.getParameter("deposit"));
+                account.deposit(deposit);
+            }
+            else
+            {
+                account.close();
+            }
             out.println(account.getBalance());
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.err.println("Something went wrong: " + ex.getMessage());
         }
     }
-   
+
 }
--- test/model/AccountTest.java
+++ test/model/AccountTest.java
@@ -46,9 +46,12 @@ public class AccountTest {
 
     @Test
     public void testGetBalanceIs100AfterDeposit500ThenWithdraw400() {
-        double cash = 400.0;
+        double cash = 500.0;
         Account instance = new Account();
+        instance.deposit(cash);
+         cash = 400.0;
         instance.withdraw(cash);
+        
         double expResult = 100.0;
         double result = instance.getBalance();
         assertEquals(expResult, result, 0.0);
